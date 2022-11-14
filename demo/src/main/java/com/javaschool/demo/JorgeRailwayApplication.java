4de0010f15e5eeaf3f9bbac8e10a1bc6f664e4e2
T-Apia <112932321+T-Apia@users.noreply.github.com>
2022-11-14 16:08:24 +0100

    Initial commit

---
+++ demo/src/main/java/com/javaschool/demo/JorgeRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.javaschool.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JorgeRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JorgeRailwayApplication.class, args);
+	}
+
+}
